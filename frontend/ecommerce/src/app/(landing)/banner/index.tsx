243eb98fbf88c8939320072acf4b653b3872ec59
psubhajit <psubhajit14@gmail.com>
2025-10-22 17:24:45 +0530

    Banner added with image responsiveness and testimonial modified

--- frontend/ecommerce/src/app/(landing)/banner/index.tsx
+++ frontend/ecommerce/src/app/(landing)/banner/index.tsx
@@ -1,11 +1,49 @@
-import { Box } from "@mantine/core";
+import {Box} from "@mantine/core";
+import Image from 'next/image'
+import {Carousel, CarouselSlide} from "@mantine/carousel";
+import Link from "next/link";
+import {BannerImageItem} from "@/constants/types";
 
+const BannerItems: BannerImageItem[] = [
+    {
+        imageUrl: "/assets/Banner1.png",
+        href: "/featured"
+    },
+    {
+        imageUrl: "/assets/Banner1.png",
+        href: "/featured"
+    },
+    {
+        imageUrl: "/assets/Banner1.png",
+        href: "/featured"
+    }
+]
 const Banner = () => {
-  return (
-    <Box bg="primary.0" py={200} px={24}>
-      Banner
-    </Box>
-  );
+    return (
+        <Box w={"100%"} p={0} m={0}>
+            <Carousel
+                withIndicators
+                emblaOptions={{
+                    loop: true,
+                }}
+            >
+                {BannerItems.map((banner) => (
+                    <CarouselSlide key={banner.imageUrl}>
+                        <Link href={banner.href}>
+                            <Image
+                                src={"/assets/Banner1.png"}
+                                width={1920}
+                                height={1080}
+                                style={{width: "100%", height: "auto"}}
+                                alt={"Banner"}
+                            />
+                        </Link>
+                    </CarouselSlide>
+                ))}
+            </Carousel>
+
+        </Box>
+    );
 };
 
 export default Banner;
--- frontend/ecommerce/src/constants/types.ts
+++ frontend/ecommerce/src/constants/types.ts
@@ -1,22 +1,26 @@
-import { IconProp } from "@fortawesome/fontawesome-svg-core";
+import {IconProp} from "@fortawesome/fontawesome-svg-core";
 
 export type NavItem = {
-  label: string;
-  href: string;
+    label: string;
+    href: string;
 };
 export type PopoverContentItemProps = {
-  label: string;
-  href: string;
-  icon: IconProp;
+    label: string;
+    href: string;
+    icon: IconProp;
 };
 export type ProductsListingProps = {
-  id: string;
-  subCategory: string;
-  rating: number;
-  name: string;
-  basePrice: number;
-  discountedPrice?: number;
-  addedToCart: boolean;
-  addedToWishList: boolean;
-  imgUrl: string;
+    id: string;
+    subCategory: string;
+    rating: number;
+    name: string;
+    basePrice: number;
+    discountedPrice?: number;
+    addedToCart: boolean;
+    addedToWishList: boolean;
+    imgUrl: string;
 };
+export type BannerImageItem = {
+    imageUrl: string;
+    href: string;
+}
